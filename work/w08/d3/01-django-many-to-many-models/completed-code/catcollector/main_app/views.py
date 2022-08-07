31b1ceec58feb761447b8e7b2f1c952d1ea8a579
Jim Clark <jim.clark@generalassemb.ly>
2022-08-07 10:24:30 -0700

    Add w08/d3

---
+++ work/w08/d3/01-django-many-to-many-models/completed-code/catcollector/main_app/views.py
@@ -0,0 +1,83 @@
+from secrets import token_bytes
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Cat, Toy
+from .forms import FeedingForm
+
+
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def cats_index(request):
+  cats = Cat.objects.all()
+  return render(request, 'cats/index.html', {'cats': cats})
+
+def cats_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  # instantiate FeedingForm to be rendered within
+  # the detail.html template
+  feeding_form = FeedingForm()
+  toy_ids = cat.toys.all().values_list('id')
+  toys = Toy.objects.exclude(id__in=toy_ids)
+  return render(request, 'cats/detail.html', {
+    'cat': cat,
+    'feeding_form': feeding_form,
+    'toys': toys
+  })
+
+# Class-Based View (CBV)
+class CatCreate(CreateView):
+  model = Cat
+  fields = ['name', 'breed', 'description', 'age']
+
+class CatUpdate(UpdateView):
+  model = Cat
+  fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats/'
+
+def add_feeding(request, cat_id):
+  #create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  #check if form is valid
+  if form.is_valid():
+    # don't want to try to save the feeding
+    # until the cat_id has been assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('detail', cat_id=cat_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
+
+def assoc_toy(request, cat_id, toy_id):
+  cat = Cat.objects.get(id=cat_id)
+  cat.toys.add(toy_id)
+  return redirect('detail', cat_id=cat_id)
+
+def unassoc_toy(request, cat_id, toy_id):
+  cat = Cat.objects.get(id=cat_id)
+  cat.toys.remove(toy_id)
+  return redirect('detail', cat_id=cat_id)
---
+++ work/w08/d3/02-03-finch-collector-lab-part-5.md
@@ -0,0 +1,35 @@
+<img src="https://images.unsplash.com/photo-1600981806713-d141a32a4f7b" width="900">
+
+# Finch Collector Lab - Part 5
+
+## Intro
+
+Today in the **Django - Many:Many Relationships** lesson, you learned how to add another model that demonstrates working with many-to-many relationships in Django. 
+
+In this lab, you'll do the same, except for a data resource of your choosing.
+
+We have chosen to collect Finches in this example!
+
+FYI, future lessons will expand upon the `catcollector` project, and the labs will expand upon the `finchcollector` project!
+
+#### The final version of `finchcollector` (parts 1 thru 5 combined) will be a deliverable, so do each part and don't fall behind.
+
+
+## Exercises
+
+Following the steps we took in the **Django - Many:Many Relationships lesson**, complete the following exercises:
+
+1. Create another model that will have a Many:Many relationship with your main data entity.
+	- Don't forget to add a ManyToManyField on one of the Models.
+
+2. Implement similar functionality to the Toy model in catcollector, including the following User Stories:
+	- AAU, I want to see a list displaying all of the toys in the database.
+
+	- AAU, I want to be able to add a Toy to list of Toys.
+
+	- AAU, when I visit the `detail` page for a finch, I want to see a list of toys belonging to the finch, and also any available toys that I can add to the finch.
+
+
+## Deliverable?
+
+#### This is the final version of `finchcollector` (parts 1 thru 5 combined), so once finished submit your work using the form as explained in the class readme.
