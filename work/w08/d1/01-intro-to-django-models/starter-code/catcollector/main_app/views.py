b097ac6259e3e843c92d6a364a06c901c9192ddb
Jim Clark <jim.clark@generalassemb.ly>
2022-08-07 07:49:10 -0700

    Update

--- work/w08/d1/01-intro-to-django-models/starter-code/catcollector/main_app/views.py
+++ work/w08/d1/01-intro-to-django-models/starter-code/catcollector/main_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-# Create your views here.
+from django.http import HttpResponse
 
-# Add the Cat class & list and view function below the imports
 class Cat:  # Note that parens are optional if not inheriting from another class
   def __init__(self, name, breed, description, age):
     self.name = name
@@ -9,17 +8,20 @@ class Cat:  # Note that parens are optional if not inheriting from another class
     self.description = description
     self.age = age
 
-cats = [ 
+cats = [
   Cat('Lolo', 'tabby', 'foul little demon', 3),
   Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
   Cat('Raven', 'black tripod', '3 legged cat', 4)
 ]
 
+# Create your views here.
 def home(request):
-  return render(request, 'home.html')
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
   return render(request, 'about.html')
 
 def cats_index(request):
-  return render(request, 'cats/index.html', {'cats': cats})
+  return render(request, 'cats/index.html', {
+    'cats': cats
+  })
