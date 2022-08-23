e837a6366765bd1d89b64719f70e257b30d7b7f0
Jim Clark <jim.clark@generalassemb.ly>
2022-08-23 10:15:13 -0700

    Add w10/d4

---
+++ work/w10/d4/01-intro-spas-and-mern-stack/completed-code/mern-infrastructure/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+require('dotenv').config();
+// Connect to db after the dotenv above
+require('./config/database');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Put all API routes here (before the catch-all)
+
+// "catch-all" route that will match all GET requests
+// that don't match an API route defined above
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`);
+});
---
+++ work/w10/d4/01-intro-spas-and-mern-stack/completed-code/mern-infrastructure/src/pages/App/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className="App">
+      App
+    </main>
+  );
+}
+
+export default App;
